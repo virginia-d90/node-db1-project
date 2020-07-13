cfd5df9471c757cad7675290eeccd5984ad38aa4
Virginia Davenport <virginialdport@gmail.com>
2020-07-13 14:55:44 -0600

    put is working

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -51,7 +51,22 @@ router.post('/', (req, res) => {
 
 
 router.put('/:id', (req, res) => {
-    
+    const { id } = req.params;
+    const changes = req.body;
+
+    db("accounts")
+        .where({id})
+        .update(changes)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({data: count})
+            } else {
+                res.status(404).json({message: "there was no record to update"})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: 'could not update account'})
+        })
 })
 
 router.delete('/:id', (req, res) => {
